6807a7a2ee3128d9706fd4c02e92c8b52e390891
AhmedElmahhdy <ahmedalaa151002@gmail.com>
2024-11-04 23:06:33 +0200

    link db host and add vercel file configration

--- db/db.conection.js
+++ db/db.conection.js
@@ -1,9 +1,13 @@
-import mongoose from "mongoose";
+import { connect } from "mongoose";
 
-export const DB_connection=async()=>{
-   await mongoose.connect(process.env.DB_CONNECTION)
-   .then(()=>{console.log("db connected")})
-   .catch((error)=>{console.log("connection error",error);
-   });
-   
+
+ export const DB_connection = async () => {
+  //mongodb+srv://ahmed:ahmed123@cluster1.z8nl0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1/Gym-System
+    await connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pbntz.mongodb.net/Gym-System`)
+  .then(() => {
+    console.log('Successfully connected to the MongoDB database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database', error);
+  });
 }
