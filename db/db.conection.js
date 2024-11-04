f974502a84f0061593e3047a4ff55a32e4299817
Ahmed Alaa <118923881+AhmedElmahhdy@users.noreply.github.com>
2024-11-04 23:20:13 +0200

    Update db.conection.js

--- db/db.conection.js
+++ db/db.conection.js
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 
  export const DB_connection = async () => {
   //mongodb+srv://ahmed:ahmed123@cluster1.z8nl0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1/Gym-System
-    await connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pbntz.mongodb.net/Gym-System`)
+    await connect(`mongodb+srv://ahmed:ahmed123@cluster0.pbntz.mongodb.net/Gym-System`)
   .then(() => {
     console.log('Successfully connected to the MongoDB database');
   })
