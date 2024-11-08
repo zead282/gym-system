710d28c24594fb24b09ceaac9f3b9aec22728b69
AhmedElmahhdy <ahmedalaa151002@gmail.com>
2024-11-08 17:05:47 +0200

    add atls connection

--- db/db.conection.js
+++ db/db.conection.js
@@ -2,8 +2,8 @@ import { connect } from "mongoose";
 
 
  export const DB_connection = async () => {
-  //mongodb+srv://ahmed:ahmed123@cluster1.z8nl0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1/Gym-System
-    await connect("mongodb://mongo:bULFhEynGIUlbTbiHvZhBJlnKyoXaqnL@junction.proxy.rlwy.net:40715")
+  //mongodb+srv://ahmed:123@cluster1.z8nl0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1/Gym-System
+    await connect("mongodb+srv://ahmed:123@cluster0.pbntz.mongodb.net//gym")
   .then(() => {
     console.log('Successfully connected to the MongoDB database');
   })
