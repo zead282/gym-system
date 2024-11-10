8e27cbf971e1810c50c3576dd45ffd60bd79143e
AhmedElmahhdy <ahmedalaa151002@gmail.com>
2024-11-11 00:48:18 +0200

    handle membership id

--- src/utils/generate-unique-str.js
+++ src/utils/generate-unique-str.js
@@ -5,7 +5,7 @@ import { customAlphabet } from 'nanoid'
 
 const generateUniqueString = (length) => {
 
-    const nanoid = customAlphabet('12345asdfgh', length || 13)
+    const nanoid = customAlphabet('123456789', length || 13)
     return nanoid()
 }
 
